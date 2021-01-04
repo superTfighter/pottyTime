c8a13975f210e97d13017c65ffff98e99824c981
LexTomi <xeltomi71@gmail.com>
2021-01-04 14:55:14 +0100

    Cleanup

--- app/src/main/java/com/example/pottytime/fragments/ToiletListFragment.kt
+++ app/src/main/java/com/example/pottytime/fragments/ToiletListFragment.kt
@@ -3,17 +3,13 @@ package com.example.pottytime.fragments
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.os.Parcel
-import android.os.Parcelable
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.GridLayoutManager
+
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pottytime.R
